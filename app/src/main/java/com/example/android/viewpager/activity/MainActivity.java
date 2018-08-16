82f19a6db15a5fce3fb6d33cca6b05ebb2ebc668
keshu <keshuvodedara@gmail.com>
2018-08-16 14:42:41 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/viewpager/activity/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.android.viewpager.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.example.android.viewpager.R;
+import com.example.android.viewpager.adepter.ViewPagerAdapter;
+import com.example.android.viewpager.fragment.OneFragment;
+import com.example.android.viewpager.fragment.ThreeFragment;
+import com.example.android.viewpager.fragment.TwoFragment;
+
+public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new OneFragment(), "ONE");
+        adapter.addFragment(new TwoFragment(), "TWO");
+        adapter.addFragment(new ThreeFragment(), "THREE");
+        viewPager.setAdapter(adapter);
+    }
+
+
+}
